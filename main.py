49f169b5b2c6bdf53024ef7cedfc7d7451969dd5
brunokemel <br.kemel@gmail.com>
2025-11-05 09:49:31 -0300

    retirando status_code | concertando variaveis

--- main.py
+++ main.py
@@ -20,18 +20,18 @@ def gerar_thumbnail(
 ):
     try:
         # Baixar o vídeo da URL fornecida
-        response = requests.get(url, stream=True)
-        if response.satatus_code != 200:
-            return JSONResponse(status_code=400, content={"error": "Não foi possível baixar o vídeo."} ,status_code=400)
+        response = requests.get(url, stream=True, timeout=10)
+        if response.status_code != 200:
+            return JSONResponse(status_code=400, content={"error": "Não foi possível baixar o vídeo."})
         
-        vide_path = os.path.join(OUTPUT_DIR, "temp_video.mp4")
-        with open(vide_path, 'wb') as f:
+        video_path = os.path.join(OUTPUT_DIR, "temp_video.mp4")
+        with open(video_path, 'wb') as f:
             for chunk in response.iter_content(chunk_size=8192):
                 f.write(chunk)
 
 
         # Captura o frame
-        video = cv2.VideoCapture(vide_path)
+        video = cv2.VideoCapture(video_path)
         fps = video.get(cv2.CAP_PROP_FPS)
         frame_target = int(fps * tempo_segundos)
         video.set(cv2.CAP_PROP_POS_FRAMES, frame_target)
@@ -39,16 +39,16 @@ def gerar_thumbnail(
         sucesso, frame = video.read()
 
         if not sucesso:
-            return JSONResponse(status_code=400, content={"erro": "Não foi possível capturar o frame."}, status_code=400)
+            return JSONResponse(status_code=400, content={"erro": "Não foi possível capturar o frame."})
         
         thumbnail_path = os.path.join(OUTPUT_DIR, "thumbnail.jpg")
         cv2.imwrite(thumbnail_path, frame)
 
         video.release()
-        os.remove(vide_path) # limpa o arquivo temporário
+        os.remove(video_path) # limpa o arquivo temporário
 
         # Retorna a imagem gerada
         return FileResponse(thumbnail_path, media_type="image/jpeg", filename="thumbnail.jpg")
     
     except Exception as e:
-        return JSONResponse(status_code=500, content={"erro": str(e)}, status_code=500)
+        return JSONResponse(status_code=500, content={"erro": str(e)})
